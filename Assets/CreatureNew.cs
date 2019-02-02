715e98faccca57039de199b0edba83e6682e31de
liammoroney <liam.moroney14@gmail.com>
2019-02-02 09:27:54 +0000

    Created new simulator and creature/genome script. Favor creatures who move the furthest.

---
+++ Assets/CreatureNew.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureNew : MonoBehaviour
+{
+    public List<HingeJoint> legs = new List<HingeJoint>();
+
+    public GameObject head;
+    private Vector3 initialPosition;
+
+    // public float a, b, c, d;
+    public struct Gene
+    {
+        public float a, b, c, d;
+
+        public Gene Mutate()
+        {
+            Gene gene = new Gene();
+            gene.a = this.a + Random.Range(0, 0.1f);
+            gene.b = this.b + Random.Range(0, 0.1f);
+            gene.c = this.c + Random.Range(0, 0.1f);
+            gene.d = this.d + Random.Range(0, 0.1f);
+            return gene;
+        }
+    }
+
+    public float GetFitness()
+    {
+        return Vector3.Distance(initialPosition, head.transform.position);
+    }
+
+    public Gene gene = new Gene();
+
+    private float outerExtent = 60;
+    private float innerExtent = -60;
+
+    [Range(-1, +1)]
+    private float position = +1;
+
+    void Awake()
+    {
+        initialPosition = head.transform.position;
+        gene.a = Random.Range(0, 1f);
+        gene.b = Random.Range(0, 1f);
+        gene.c = Random.Range(0, 1f);
+        gene.d = Random.Range(0, 1f);
+        // a = Random.Range(0, 1f);
+        // b = Random.Range(0, 1f);
+        // c = Random.Range(0, 1f);
+        // d = Random.Range(0, 1f);
+    }
+
+    public float EvaluateAt(float time)
+    {
+        return (gene.b - gene.a) / 2 * (1 + Mathf.Sin((time + gene.c) * Mathf.PI * 2 / gene.d)) + gene.a;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i = 0; i < legs.Count; i++)
+        {
+            JointSpring spring = legs[i].spring;
+            spring.targetPosition = Scale(-1F, 1F, innerExtent, outerExtent, EvaluateAt(System.DateTime.Now.Millisecond));
+            legs[i].spring = spring;
+        }
+    }
+
+    float Scale(float OldMin, float OldMax, float NewMin, float NewMax, float OldValue)
+    {
+
+        float OldRange = (OldMax - OldMin);
+        float NewRange = (NewMax - NewMin);
+        float NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin;
+
+        return (NewValue);
+    }
+}
---
+++ Assets/SimulatorNew.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulatorNew : MonoBehaviour
+{
+    public GameObject p_creature;
+    public int populationSize = 20;
+
+    private Vector3 start = new Vector3(0, 2, 0);
+    private CreatureNew.Gene bestGene;
+    public List<GameObject> population = new List<GameObject>();
+
+    void Start()
+    {
+        bestGene = new CreatureNew.Gene();
+        bestGene.a = Random.Range(0, 1f);
+        bestGene.b = Random.Range(0, 1f);
+        bestGene.c = Random.Range(0, 1f);
+        bestGene.d = Random.Range(0, 1f);
+
+        InvokeRepeating("RunSimulation", 0, 5f);
+    }
+
+    void RunSimulation()
+    {
+        float bestFitness = 0;
+        CreatureNew.Gene bestGene = new CreatureNew.Gene();
+        // Debug.Log("pop: " + population.Count);
+        // for (int i = 0; i < population.Count; i++)
+        foreach(GameObject c in population)
+        {
+            // GameObject c = population[i];
+            float fitness = c.GetComponent<CreatureNew>().GetFitness();
+            if (fitness > bestFitness)
+            {
+                bestFitness = fitness;
+                bestGene = c.GetComponent<CreatureNew>().gene;
+            }
+            // Debug.Log(fitness);
+            Destroy(c);
+        }
+        population.Clear();
+
+        Debug.Log("best: " + bestFitness);
+
+        for (int i = 0; i < populationSize; i++)
+        {
+            GameObject creature = Instantiate(p_creature, start + new Vector3(-2 * i, 0, 0), Quaternion.identity);
+            creature.GetComponent<CreatureNew>().gene = bestGene.Mutate();
+            population.Add(creature);
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+}
