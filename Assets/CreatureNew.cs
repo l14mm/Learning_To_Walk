fd30613bf68567eaa95df11ec940ca071aac20d3
liammoroney <liam.moroney14@gmail.com>
2019-02-02 10:02:28 +0000

    Changed fitness function to heavily favor creature who stay upright. Changed mutation function to mutate only one variable in the gene at once. Fixed creation of population from best creature/gene.

--- Assets/CreatureNew.cs
+++ Assets/CreatureNew.cs
@@ -9,25 +9,71 @@ public class CreatureNew : MonoBehaviour
     public GameObject head;
     private Vector3 initialPosition;
 
-    // public float a, b, c, d;
     public struct Gene
     {
         public float a, b, c, d;
 
+        public void Init()
+        {
+            this.a = Random.Range(-1f, 1f);
+            this.b = Random.Range(-1f, 1f);
+            this.c = Random.Range(-1f, 1f);
+            this.d = Random.Range(-1f, 1f);
+        }
+
         public Gene Mutate()
         {
             Gene gene = new Gene();
-            gene.a = this.a + Random.Range(0, 0.1f);
-            gene.b = this.b + Random.Range(0, 0.1f);
-            gene.c = this.c + Random.Range(0, 0.1f);
-            gene.d = this.d + Random.Range(0, 0.1f);
+            
+            gene.a = this.a;
+            gene.b = this.b;
+            gene.c = this.c;
+            gene.d = this.d;
+
+            float mutationScale = 2;
+            float rangeScale = 4;
+            switch (Random.Range(0, 3 + 1))
+            {
+                case 0:
+                    gene.a += Random.Range(-0.1f, 0.1f) * mutationScale;
+                    gene.a = Mathf.Clamp(a, -1f * rangeScale, +1f * rangeScale);
+                    break;
+                case 1:
+                    gene.b += Random.Range(-0.1f, 0.1f) * mutationScale;
+                    gene.b = Mathf.Clamp(b, -1f * rangeScale, +1f * rangeScale);
+                    break;
+                case 2:
+                    gene.c += Random.Range(-0.25f, 0.25f) * mutationScale;
+                    gene.c = Mathf.Clamp(c, 0.1f * rangeScale, 2f * rangeScale);
+                    break;
+                case 3:
+                    gene.d += Random.Range(-0.25f, 0.25f) * mutationScale;
+                    gene.d = Mathf.Clamp(d, -2f * rangeScale, 2f * rangeScale);
+                    break;
+            }
+
             return gene;
         }
     }
 
     public float GetFitness()
     {
-        return Vector3.Distance(initialPosition, head.transform.position);
+        // Walking score
+        float walkingScore = Vector3.Distance(initialPosition, head.transform.position);
+
+        // Balancing score - favor creatures heavily based on if they stay upright
+        bool headUp =
+            head.transform.eulerAngles.z < 0 + 30 ||
+            head.transform.eulerAngles.z > 360 - 30;
+        bool headDown =
+            head.transform.eulerAngles.z > 180 - 45 &&
+            head.transform.eulerAngles.z < 180 + 45;
+
+        return
+            walkingScore
+            * (headUp ? 2f : 1f)    // Double score if UP
+            * (headDown ? 0.5f : 1f)    // Half score if DOWN
+            ;
     }
 
     public Gene gene = new Gene();
--- Assets/SimulatorNew.cs
+++ Assets/SimulatorNew.cs
@@ -5,56 +5,53 @@ using UnityEngine;
 public class SimulatorNew : MonoBehaviour
 {
     public GameObject p_creature;
+    // Number of creatures in each population
     public int populationSize = 20;
 
+    // How long each population should be evaulated for before creating a new one
+    public int simulationTime = 10;
+
     private Vector3 start = new Vector3(0, 2, 0);
-    private CreatureNew.Gene bestGene;
+    private CreatureNew.Gene parentGene;
     public List<GameObject> population = new List<GameObject>();
 
     void Start()
     {
-        bestGene = new CreatureNew.Gene();
-        bestGene.a = Random.Range(0, 1f);
-        bestGene.b = Random.Range(0, 1f);
-        bestGene.c = Random.Range(0, 1f);
-        bestGene.d = Random.Range(0, 1f);
+        // Creature random parent for initial population
+        parentGene = new CreatureNew.Gene();
+        parentGene.Init();
 
-        InvokeRepeating("RunSimulation", 0, 5f);
+        InvokeRepeating("RunSimulation", 0, simulationTime);
     }
 
     void RunSimulation()
     {
         float bestFitness = 0;
-        CreatureNew.Gene bestGene = new CreatureNew.Gene();
-        // Debug.Log("pop: " + population.Count);
-        // for (int i = 0; i < population.Count; i++)
+        CreatureNew.Gene bestGene = parentGene;
+
+        // Find creature with best gene/fitness
+        // then remove population
         foreach(GameObject c in population)
         {
-            // GameObject c = population[i];
             float fitness = c.GetComponent<CreatureNew>().GetFitness();
             if (fitness > bestFitness)
             {
                 bestFitness = fitness;
                 bestGene = c.GetComponent<CreatureNew>().gene;
             }
-            // Debug.Log(fitness);
             Destroy(c);
         }
         population.Clear();
 
-        Debug.Log("best: " + bestFitness);
+        Debug.Log("bestfitness: " + bestFitness);
 
+        // Create new population mutated from best creature/gene from last population
         for (int i = 0; i < populationSize; i++)
         {
-            GameObject creature = Instantiate(p_creature, start + new Vector3(-2 * i, 0, 0), Quaternion.identity);
+            GameObject creature = Instantiate(p_creature, start + new Vector3(4 * i, 0, 0), Quaternion.identity);
             creature.GetComponent<CreatureNew>().gene = bestGene.Mutate();
             population.Add(creature);
         }
-
-    }
-
-    void Update()
-    {
 
     }
 }
